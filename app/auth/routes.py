f4e9566b7dcee06ab3d2608a25306713a91ed124
idanaroz <idanaroz@gmail.com>
2019-11-13 14:42:03 +0200

    Add mechanisms: login, logout, register, signout

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required, current_user
 
 from app.auth import authentication as at
 from app.auth.forms import RegistrationForm, LoginForm
@@ -9,6 +9,10 @@ from app.catalog.models import User
 @at.route('/register', methods=['GET', 'POST'])
 def register_user():
     form = RegistrationForm()
+    if current_user.is_authenticated:
+        flash('You are already logged-in')
+        return redirect(url_for('main.display_exercises'))
+
     if form.validate_on_submit():  # check that it is Post request and validate the data
         User.create_user(
             user=form.name.data,
@@ -23,6 +27,9 @@ def register_user():
 
 @at.route('/login', methods=['GET', 'POST'])
 def do_the_login():
+    if current_user.is_authenticated:
+        flash('You are already logged-in')
+        return redirect(url_for('main.display_exercises'))
     form = LoginForm()
     if form.validate_on_submit():  # Post
         user = User.query.filter_by(user_email=form.email.data).first()
@@ -34,3 +41,10 @@ def do_the_login():
         return redirect(url_for('main.display_exercises'))
 
     return render_template('login.html', form=form)
+
+
+@at.route('/logout')
+@login_required
+def log_out_user():
+    logout_user()
+    return redirect(url_for('main.display_exercises'))
