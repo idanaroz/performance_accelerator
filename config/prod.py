a6e3ea02d2ea1dda4529ab48545e57a68fc5d236
idanaroz <idanaroz@gmail.com>
2019-11-14 22:03:25 +0200

    correction for deployment in heroku

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,6 @@
+import os
+
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://mysql:topsecret@localhost/accelerator'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
