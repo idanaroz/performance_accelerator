aeeecc6a77be8aa1204c8c085371b434533217d1
idanaroz <idanaroz@gmail.com>
2019-12-07 18:29:45 +0200

    make it better

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 from app import create_app, db
 from app.catalog.models import User
+from sqlalchemy import exc
 
 flask_app = create_app('prod')
 
 with flask_app.app_context():
     db.create_all()
-    if not User.query.filter_by(user_name='harry').first():
-        User.create_user(user='harry',
-                         email="harry@potters.com",
-                         password="secret")
+    try:
+        if not User.query.filter_by(user_name='harry').first():
+            User.create_user(user='harry',
+                             email="harry@potters.com",
+                             password="secret")
+    except exc.IntegrityError:
+        flask_app.run()
+
 
 if __name__ == '__main__':
     flask_app.run()
